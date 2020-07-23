42f31c1d140c4fd1d48b26901dbbbedb94302db7
Minh Huy <859@hobertonschool.com>
2020-07-23 01:17:21 -0400

    audio

---
+++ 0x08-unity-audio/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+    
+    public GameObject MainCamera;
+    public GameObject Timer;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        MainCamera.SetActive(true);
+        Timer.SetActive(true);
+        Player.GetComponent<PlayerController>().enabled = true;
+        gameObject.SetActive(false);
+    }
+}
