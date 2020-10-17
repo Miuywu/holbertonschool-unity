c8d7df094c23b1968b82bf1f6e9d5b86453ea7bf
Miuywu <859@holbertonschool.com>
2020-10-17 00:13:02 -0400

    arvr

---
+++ 0x0A-unity-360_video_tour/Assets/Scripts/VideoManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+
+    public VideoClip[] videos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+    }
+    public void Play()
+    {
+        videoPlayer.Play();
+    }
+
+    public void Pause()
+    {
+        videoPlayer.Pause();
+    }
+    public void Stop()
+    {
+        videoPlayer.Stop();
+    }
+
+
+
+    public void livingVideo()
+    {
+        videoPlayer.source = VideoSource.VideoClip;
+        videoPlayer.clip = videos[0];
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += VideoPlayer_prepareCompleted;
+    }
+
+    public void cuboVideo()
+    {
+        videoPlayer.source = VideoSource.VideoClip;
+        videoPlayer.clip = videos[1];
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += VideoPlayer_prepareCompleted;
+    }
+
+    public void cantinaVideo()
+    {
+        videoPlayer.source = VideoSource.VideoClip;
+        videoPlayer.clip = videos[2];
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += VideoPlayer_prepareCompleted;
+    }
+    public void mezzanineVideo()
+    {
+        videoPlayer.source = VideoSource.VideoClip;
+        videoPlayer.clip = videos[3];
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += VideoPlayer_prepareCompleted;
+    }
+
+    private void VideoPlayer_prepareCompleted(VideoPlayer source)
+    {
+        Play();
+    }
+}
